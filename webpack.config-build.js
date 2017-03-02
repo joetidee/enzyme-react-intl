931f5c060664ec2a05999f25cf198836c29a18e9
Joe Privett <joe@tidee.com>
2017-03-02 00:18:35 +0000

    corrected lib path in webpack config file

--- webpack.config-build.js
+++ webpack.config-build.js
@@ -13,7 +13,7 @@ module.exports = {
 	},
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '/lib')
+        path: path.resolve(__dirname, 'lib')
     },
     plugins: [
         new webpack.IgnorePlugin(/react\/addons/),
